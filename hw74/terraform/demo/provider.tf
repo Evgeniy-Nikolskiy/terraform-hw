0cf79e666637bbf56eb869797b5f3a65b725206b
Evgeniy Nikolskiy <weiruk92@gmail.com>
2022-01-13 21:16:52 +0300

    hw74-v1.0

--- hw74/terraform/demo/provider.tf
+++ hw74/terraform/demo/provider.tf
@@ -4,6 +4,7 @@
 # Cloud where your token is already securely stored in your workspace!
 
 variable "provider_token" {
+  default = "F9HMvHnbyyDE5g.atlasv1.K9OkCDSfjSO67x5v0adXk80HU0QBJhxdavGJzk2cXbfO0Q8zjb5ayBjoaVBgytYgwkw"
   type = string
   sensitive = true
 }
